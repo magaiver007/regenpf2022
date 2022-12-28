a228586a7b327514287090e72f9d92d88c685da1
StefanosOuntrakis <sountrakis@gmail.com>
2022-12-28 17:21:20 +0200

    First upload

---
+++ acmeflix/src/test/java/com/movies/acmeflix/AcmeflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.acmeflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
