e9162824e4931d93cf49043a90e03a4d111a3e89
dimosthenis-k <dkarampinas96@gmail.com>
2023-01-08 19:12:28 +0200

    changed SerieRepository.java to SeriesRepository.java

--- acmeflix/src/main/java/com/movies/acmeflix/model/MovieModel/repository/SeriesRepository.java
+++ acmeflix/src/main/java/com/movies/acmeflix/model/MovieModel/repository/SeriesRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface SerieRepository extends JpaRepository<Series, Long> {
+public interface SeriesRepository extends JpaRepository<Series, Long> {
 
     List<Series> findTopByTotalViews(int totalViews);
 
