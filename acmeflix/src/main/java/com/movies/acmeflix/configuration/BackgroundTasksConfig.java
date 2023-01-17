3d2c115ac90b9fd18fd08fb1324f7c160849bbc9
StefanosOuntrakis <sountrakis@gmail.com>
2023-01-17 09:40:03 +0200

    changes 16-1,metria ulopoihsh sto erwthma gia to backup

--- acmeflix/src/main/java/com/movies/acmeflix/configuration/BackgroundTasksConfig.java
+++ acmeflix/src/main/java/com/movies/acmeflix/configuration/BackgroundTasksConfig.java
@@ -1,8 +1,6 @@
 package com.movies.acmeflix.configuration;
 
 import com.opencsv.CSVWriter;
-import org.h2.tools.*;
-import org.h2.command.dml.Call;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +10,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.sql.*;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -35,29 +32,27 @@ public class BackgroundTasksConfig {
         table.add("SERIES");
         table.add("CREW_MEMBERS");
 
-        File file = new File("C:/Users/stef6/IdeaProjects/regenpf2022/acmeflix/MyCSV.txt");
+        File file = new File("MyCSV.txt");
         try {
             FileWriter outputfile = new FileWriter(file);
             CSVWriter writer = new CSVWriter(outputfile);
             Statement stmt = DriverManager.getConnection("jdbc:h2:file:~/acmeflix","sa","sa").createStatement();
-            for(int i=0; i<table.size();i++){
-                ResultSet rs = stmt.executeQuery ("SELECT * FROM "+table.get(i)+"");
+            for (String s : table) {
+                ResultSet rs = stmt.executeQuery("SELECT * FROM " + s + "");
                 ResultSetMetaData rsmd = rs.getMetaData();
                 int column_count = rsmd.getColumnCount();
-                writer.writeNext(new String[]{"--------------------------" + table.get(i) + "-----------------"});
-                while (rs.next()){
+                writer.writeNext(new String[]{"--------------------------" + s + "-----------------"});
+                while (rs.next()) {
                     String[] data = new String[column_count];
-                    for (int j=0;j<column_count;j++)
-                    {
-                        data[j] = rs.getString(j+1);
+                    for (int j = 0; j < column_count; j++) {
+                        data[j] = rs.getString(j + 1);
                     }
-
                     writer.writeNext(data);
                 }
 
             }
 
-            LOG.info("Doing backup....", LocalDate.now());
+            LOG.info("Doing backup at ({})", LocalDateTime.now());
             writer.close();
         }
         catch (IOException e) {
